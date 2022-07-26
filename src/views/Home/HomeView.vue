85cfa5bf174dd8dfddb1c86cee2b5107987777ae
张焕标 <1140457303@qq.com>
2022-07-26 11:47:27 +0800

    uppercase issue

--- src/views/Home/HomeView.vue
+++ src/views/Home/HomeView.vue
@@ -2,7 +2,7 @@
 import CarouselComp from "./components/CarouselComp.vue";
 import NoticeComp from "./components/NoticeComp.vue";
 import HotMarketsComp from "./components/HotMarketsComp.vue";
-import TradingVolume from "./components/TradingVolume.vue";
+import TradingVolume from "./components/TradingVolum.vue";
 import MarketsComp from "./components/Markets/MarketsComp.vue";
 </script>
 
